065ccd297cd5bb01da6ab9e09a411e1f145128df
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2023-05-14 14:35:16 +0300

    update and create

--- assets/js/box .js
+++ assets/js/box .js
@@ -4698,35 +4698,3 @@ Prism.languages.iecst = {
   }
 })();
 
-const copyButtonLabel = "Copy Code";
-
-// use a class selector if available
-let blocks = document.querySelectorAll("pre");
-
-blocks.forEach((block) => {
-  // only add button if browser supports Clipboard API
-  if (navigator.clipboard) {
-    let button = document.createElement("button");
-
-    button.innerText = copyButtonLabel;
-    block.appendChild(button);
-
-    button.addEventListener("click", async () => {
-      await copyCode(block);
-    });
-  }
-});
-
-block.setAttribute("tabindex", 0);
-async function copyCode(block) {
-  let code = block.querySelector("code");
-  let text = code.innerText;
-
-  await navigator.clipboard.writeText(text);
-}
-block.setAttribute("tabindex", 0);
-button.innerText = "Code Copied";
-
-setTimeout(()=> {
-  button.innerText = copyButtonLabel;
-},700)
