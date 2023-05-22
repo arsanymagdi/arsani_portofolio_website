06b97d7fdf84c8fbdd13c8294dbff655657cc183
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2023-05-22 12:01:00 +0300

    update project

---
+++ assets/Files/script.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+            e.target.classList.toggle("chaecked");
+            saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
+  // Trigger button click event on Enter key press
+  document.addEventListener("keydown", function(event) {
+    // Check if the Enter key is pressed
+    if (event.key === "Enter") {
+      // Trigger the button click event
+      document.getElementById("myButton").click();
+    }
+  });
