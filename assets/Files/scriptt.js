6b16b36a35fdc1a1b89b288f492d314e8c982af6
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2023-05-28 01:20:58 +0300

    update

--- assets/Files/scriptt.js
+++ assets/Files/scriptt.js
@@ -4,12 +4,13 @@ const lenght = 12;
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbol = "@#$%-*/\?";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
 
 const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
     let password = "";
+    password += upperCase[Math.floor(Math.random() * uppercase.length)];
     password += upperCase[Math.floor(Math.random() * upperCase.lenght)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.lenght)];
     password += number[Math.floor(Math.random() * number.lenght)];
