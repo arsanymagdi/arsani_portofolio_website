4a862db0dd0a7c22a3d7f865798b27b629ccabe8
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2023-06-12 22:42:02 +0300

    update

--- assets/Files/script.js
+++ assets/Files/script.js
@@ -3,13 +3,7 @@ const createBtn = document.querySelector('.btn');
 let notes = document.querySelectorAll('.input-box');
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-const passwordBox = document.getElementById("password");
-const lenght = 12;
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "@#$%-*/\?";
 
 function showNotes(){
     notesContainer.innerHTML = localStorage.getItem("notes");
@@ -96,17 +90,30 @@ showTask();
       document.getElementById("myButton").click();
     }
   });
+  const passwordBox = document.getElementById("password");
+  const lenght = 12;
+  
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const number = "0123456789";
+  const symbol = "@#$%^&*()_+~|}{[]></-=";
+  
   const allChars = upperCase + lowerCase + number + symbol;
-
+  
   function createPassword(){
       let password = "";
-      password += upperCase[Math.floor(Math.random() * upperCase.lenght)];
-      password += lowerCase[Math.floor(Math.random() * lowerCase.lenght)];
-      password += number[Math.floor(Math.random() * number.lenght)];
-      password += symbol[Math.floor(Math.random() * symbol.lenght)];
+      password += upperCase[Math.floor(Math.random() * upperCase.length)];
+      password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+      password += number[Math.floor(Math.random() * number.length)];
+      password += symbol[Math.floor(Math.random() * symbol.length)];
   
-      while(lenght > password.lenght){
-          password += allChars[Math.floor(Math.random() * allChars.lenght)]; 
+      while(lenght > password.length){
+          password += allChars[Math.floor(Math.random() * allChars.length)]; 
       }
       passwordBox.value = password;
+  }
+  
+  function copyPassword(){
+      passwordBox.select();
+      document.execCommand("copy");
   }
