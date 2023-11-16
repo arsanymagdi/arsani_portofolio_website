cc0dcefbe9c921c5b8d57037d5eebc3b9d3f78ab
Arsany Magdy <109736550+arsanymagdi@users.noreply.github.com>
2023-11-16 16:03:19 -0600

    up chowmore

--- assets/js/moreless.js
+++ assets/js/moreless.js
@@ -3,3 +3,19 @@ function showtext()
 var text="Here is some text that I want added to the HTML file";
 document.getElementById("textarea").innerHTML=text;
 }
+var status = "less";
+
+function toggleText()
+{
+    var text="Here is some text that I want added to the HTML file";
+    
+    if (status == "less") {
+        document.getElementById("textArea").innerHTML=text;
+        document.getElementById("toggleButton").innerText = "See Less";
+        status = "more";
+    } else if (status == "more") {
+        document.getElementById("textArea").innerHTML = "";
+        document.getElementById("toggleButton").innerText = "See More";
+        status = "less"
+    }
+}
